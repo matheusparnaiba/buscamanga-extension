39cce766cb90343be8d634b3bbf3cfa70f148106
Matheus Parnaíba <matheus_parnaiba@hotmail.com>
2026-06-22 14:50:57 -0300

    limpeza de arquivos desnecessarios

--- check_body.cjs
+++
@@ -1,5 +0,0 @@
-const html = require('fs').readFileSync('hipertoon_rendered.html', 'utf8');
-const cheerio = require('cheerio');
-const $ = cheerio.load(html);
-const text = $('body').text().replace(/\s+/g, ' ');
-console.log(text.substring(0, 1000));
--- parse_mobile.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const cheerio = require('cheerio');
-const html = fs.readFileSync('mobile.html', 'utf8');
-const $ = cheerio.load(html);
-console.log($('.popular-item-wrap').first().html());
