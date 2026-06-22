94e1712474f56fbec7657a8707f7ab389f3d4ac1
Matheus Parnaíba <matheus_parnaiba@hotmail.com>
2026-06-22 01:19:59 -0300

    Limpeza de arquivos e scripts de teste

--- mangalivre_scraper.cjs
+++
@@ -1,8 +0,0 @@
-const cheerio = require('cheerio');
-fetch('https://mangalivre.to/manga/chainsaw-man-pt-br/capitulo-232/', {headers: {'User-Agent': 'Mozilla/5.0'}})
-  .then(r => r.text())
-  .then(t => {
-    const $ = cheerio.load(t);
-    console.log('Chapter Images (.page-break img):', $('.page-break img, .reading-content img').length);
-    console.log('First Image:', $('.page-break img, .reading-content img').first().attr('src'));
-  });
