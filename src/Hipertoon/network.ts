e9e7f545df3cea34d0ede511c11eafb9214705e1
Matheus Parnaíba <matheus_parnaiba@hotmail.com>
2026-06-23 22:54:13 -0300

    fix: Resolve loop do Cloudflare utilizando o User-Agent nativo do dispositivo

--- src/Hipertoon/network.ts
+++ src/Hipertoon/network.ts
@@ -10,8 +10,7 @@ export class MainInterceptor extends PaperbackInterceptor {
       ...(request.headers ?? {}),
       ...{
         referer: "https://mangaonline.blue/",
-        "user-agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "user-agent": await Application.getDefaultUserAgent(),
       },
     };
     return request;
